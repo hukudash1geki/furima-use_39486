e8eb38df804468493ef557ef076d234318b0688f
hukudash1geki <fukudashgek81@gmail.com>
2023-07-12 16:50:03 +0900

    配送先入力の保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,27 @@
 class PurchasesController < ApplicationController
+
+  def new
+    @items = Item.all
+    @item = Item.find(params[:item_id])
+    @purchase_deliverie = PurchaseDeliverie.new(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def create
+    @purchase_deliverie = PurchaseDeliverie.new(purchase_params)
+    if @purchase_deliverie.valid?
+       @purchase_deliverie.save
+       redirect_to root_path
+    else
+      @items = Item.all
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_deliverie).permit(:postal_code, :prefecture_id, :cities_and_towns, :house_number, :building_name, :telephone_number, :token).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
 end
+
--- app/models/deliverie.rb
+++ app/models/deliverie.rb
@@ -1,23 +1,5 @@
 class Deliverie < ApplicationRecord
   belongs_to :purchase
 
-  extend ActiveHash::Associations::ActiveRecordExtensions
-    belongs_to :prefecture
 
-    with_options presence: true do
-      validates :postal_code
-      validates :cities_and_towns
-      validates :house_number
-      validates :telephone_number
-    end
-
-  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Enter it as follows (e.g. 123-4567)"}
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
-  validates :telephone_number, length: { minimum: 11, too_short: "is too short" }
-  validates :telephone_number, format: { without:  /\A\d+\z/, message: "is invalid. Input only number" }
-
-  
-  with_options numericality: { other_than: 0, message: "can't be blank" } do
-    validates :prefecture_id
-  end
 end

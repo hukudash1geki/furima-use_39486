1e656b37e7d4cb769162a1c8030646fd12105691
hukudash1geki <fukudashgek81@gmail.com>
2023-07-17 21:41:41 +0900

    コードレビュー再修正

--- app/models/purchase_deliverie.rb
+++ app/models/purchase_deliverie.rb
@@ -16,13 +16,13 @@ class PurchaseDeliverie
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
   validates :telephone_number, presence: true
-  validates :telephone_number, length: { minimum: 11, too_short: 'is too short' }
+  validates :telephone_number, length: { minimum: 10, too_short: 'is too short' }
   validates :telephone_number, length: { maximum: 11 }
   validates :telephone_number, numericality: { other_than: 0, message: 'is invalid. Input only number' }
 
   def save
-    purchase = Purchase.create(item_id:, user_id:)
-    deliverie = Deliverie.create(postal_code:, prefecture_id:, cities_and_towns:,
-                                 house_number:, building_name:, telephone_number:, purchase_id: purchase.id)
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    deliverie = Deliverie.create(postal_code: postal_code, prefecture_id: prefecture_id, cities_and_towns: cities_and_towns,
+                                 house_number: house_number, building_name: building_name, telephone_number: telephone_number, purchase_id: purchase.id)
   end
 end

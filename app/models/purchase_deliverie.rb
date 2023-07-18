862d9f7cf3517a1074ef3172981c22525f81e996
hukudash1geki <fukudashgek81@gmail.com>
2023-07-18 23:33:28 +0900

    Revert "コード修正"
    This reverts commit 0f85357c24b1f5f4d3e69790406fe9337df06d18.

--- app/models/purchase_deliverie.rb
+++ app/models/purchase_deliverie.rb
@@ -15,6 +15,7 @@ class PurchaseDeliverie
 
   validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
   validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :telephone_number, presence: true
   validates :telephone_number, length: { minimum: 10, too_short: 'is too short' }
   validates :telephone_number, length: { maximum: 11 }
   validates :telephone_number, numericality: { other_than: 0, message: 'is invalid. Input only number' }
